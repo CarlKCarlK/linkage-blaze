93a53c937c6f22ff87d87efed76ea9ecbe0dee4b
Carl Kadie <carlk@msn.com>
2026-06-17 15:22:22 -0700

    Refactor clock hands definition and move to separate file for improved organization

---
+++ crates/robot-arm-cyd-clock/src/clock.lb.rs
@@ -0,0 +1,58 @@
+Linkage::start()
+    .define_param("hour", 0.0)
+    .define_param("face spin", 0.5)
+    // Common transform for the whole clock face.
+    .roll_param("face spin", -90.0, 90.0)
+    .remember("face")
+    // Face disk
+    .pen_color(Rgb888::new(33, 79, 155))
+    .disk(66.0)
+    // 12 o'clock tick
+    .recall("face")
+    .pen_width(4.5)
+    .pen_color(Rgb888::new(255, 245, 216))
+    .pen_up()
+    .remember("ticks")
+    .forward(45.0)
+    .pen_down()
+    .forward(18.0)
+    // 3 o'clock tick
+    .recall("ticks")
+    .yaw(-90.0)
+    .forward(51.0)
+    .pen_down()
+    .forward(11.0)
+    // 6 o'clock tick
+    .recall("ticks")
+    .yaw(180.0)
+    .forward(51.0)
+    .pen_down()
+    .forward(11.0)
+    // 9 o'clock tick
+    .recall("ticks")
+    .yaw(90.0)
+    .forward(51.0)
+    .pen_down()
+    .forward(11.0)
+    // Hour hand
+    .recall("face")
+    .pen_color(Rgb888::CSS_ANTIQUE_WHITE)
+    .pen_width(16.0)
+    .yaw_param("hour", 360.0, 0.0)
+    .forward(40.0)
+    // Minute hand
+    .recall("face")
+    .pen_color(Rgb888::new(69, 215, 255))
+    .pen_width(7.5)
+    .yaw_param("hour", 4320.0, 0.0)
+    .forward(52.0)
+    // Second hand
+    .recall("face")
+    .pen_color(Rgb888::new(255, 89, 72))
+    .pen_width(2.0)
+    .yaw_param("hour", 259_200.0, 0.0)
+    .forward(60.0)
+    // Hub
+    .recall("face")
+    .pen_color(Rgb888::CSS_RED)
+    .disk(8.0)
