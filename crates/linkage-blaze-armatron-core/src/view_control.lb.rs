730ee8f45e8e4cefa5d0587fcb02eb0c5fda1d8e
Carl Kadie <carlk@msn.com>
2026-06-18 09:50:03 -0700

    feat: update linkage definitions and add grid drawing logic for enhanced functionality

--- crates/linkage-blaze-armatron-core/src/view_control.lb.rs
+++ crates/linkage-blaze-armatron-core/src/view_control.lb.rs
@@ -2,6 +2,5 @@ Linkage::start()
     .define_param("x/y view", 0.5833)
     .define_param("z", 0.3)
     .pitch_param("z", 0.0, -45.0)
-    .yaw_param("x/y view", 90.0, -90.0)
-  .pen_color(Rgb888::CSS_DARK_SLATE_GRAY)
-.disk(3.0)
+    .yaw_param("x/y view", 180.0, -180.0)
+    
