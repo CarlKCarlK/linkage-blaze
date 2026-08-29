bff9dc6eb9b35ca48362ecb937fc33b8fdaec7eb
Carl Kadie <carlk@msn.com>
2026-08-29 15:56:04 -0700

    Format linkage-blaze build script

--- crates/linkage-blaze/build.rs
+++ crates/linkage-blaze/build.rs
@@ -1,13 +1,11 @@
-use std::error::Error;
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let manifest_directory = PathBuf::from(
-        env::var_os("CARGO_MANIFEST_DIR")
-            .ok_or("CARGO_MANIFEST_DIR is not set")?,
-    );
+    let manifest_directory =
+        PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").ok_or("CARGO_MANIFEST_DIR is not set")?);
     let repository_readme = manifest_directory.join("../../README.md");
     let packaged_readme = manifest_directory.join("README.md");
     let readme = if repository_readme.is_file() {
@@ -17,8 +15,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     };
 
     println!("cargo:rerun-if-changed={}", readme.display());
-    let output_readme = PathBuf::from(env::var_os("OUT_DIR").ok_or("OUT_DIR is not set")?)
-        .join("README.md");
+    let output_readme =
+        PathBuf::from(env::var_os("OUT_DIR").ok_or("OUT_DIR is not set")?).join("README.md");
     fs::copy(&readme, output_readme)?;
     Ok(())
 }
